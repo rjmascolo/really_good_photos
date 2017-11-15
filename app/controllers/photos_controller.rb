5030c953307aef2fa80da8d09c1902106b0d419c
github username <github email address>
2017-11-15 10:22:33 -0500

    photo changes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,10 @@
 class PhotosController < ApplicationController
 
   def index
-    @user = User.find_by(session[:id])
-    @photos = @user.create_photos
+    # byebug
+    @user = User.find_by(id: session[:user_id])
+    @photos = @user.get_photo_info
   end
+
 
 end

6c1e391bec3edd0010e32005dadb6ffb25a14403
github username <github email address>
2017-11-21 13:21:14 -0500

    changed database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171113183150) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.integer "category_id"
     t.string "name"
